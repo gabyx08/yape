63213ca9f1535623b18353d3e1fdd5882f4d5c97
fbk-rena <gaby.ranarz@gmail.com>
2017-07-19 11:38:15 -0500

    api pantalla 2

--- public/assets/js/number.js
+++ public/assets/js/number.js
@@ -1,34 +1,48 @@
 var cargarPagina = function(){
-  $("#numero").change(validarNumero);
+  $("#numero").keyup(validarNumero);
   $("#formNumero").submit(agregarNumero)
 };
 
 var validarNumero = function() {
   var $numero = $("#numero");
   var $boton = $("#btnNumero");
-  var $checkbox = $("#terminos");
   if($numero.val().length === 10 ){
-       validarCheckbox(); //valida el checkbox ANTES modificar el input
-      $checkbox.click(validarCheckbox); //valida el checkbox DESPUES modificar input y hacer click en el ckecbox
+      validarCheckbox(); //valida el checkbox ANTES modificar el input
+      $("#terminos").click(validarCheckbox); //valida el checkbox DESPUES modificar input y hacer click en el ckecbox
    } else {
      $boton.attr("disabled", true);
   }
-}
+};
 
 var validarCheckbox = function(){
   var $boton = $("#btnNumero");
   var $checkbox = $("#terminos");
-  //Con el método .prop() podemos obtener el valor de la propiedad checked con jquery
+  //Con el método .prop() se obtiene el valor de la propiedad checked con jquery
   if($checkbox.prop("checked")){
       $boton.removeAttr("disabled");
    } else {
      $boton.attr("disabled", true);
   }
-}
+};
 
 var agregarNumero = function(e){
   e.preventDefault();
-  alert("hola")
-}
+  var telefono = $("#numero").val();
+  var terminos = $("#terminos").prop("checked");
+
+  $.post("http://localhost:3000/api/registerNumber",{
+    "phone": telefono,
+    "terms": terminos
+  }).then(function(res){
+    console.log(res.message);
+    if(res.message === "Usuario válido"){
+      location.href = "codigo.html";
+    } else {
+      swal(res.message , "Ingresa otro número :)", "error");
+    }
+  }).catch(function(error){
+    console.log(error)
+  })
+};
 
 $(document).ready(cargarPagina);
